9b58795593e73db828eae423b35177fa5604302c
Dev-Pranav123 <ppkelkar123465@gmail.com>
2023-05-06 17:07:33 +0530

    updated candidate List code

--- src/screens/CandidateList.js
+++ src/screens/CandidateList.js
@@ -2,19 +2,20 @@ import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView, ImageBackground } from "react-native";
 import { getDatabase, ref, onValue } from "firebase/database";
 
-const CandidateList = () => { 
+const CandidateList = () => {
     const [candidateList, setCandidateList] = useState([]);
     const db = getDatabase();
     useEffect(() => {
         const startCountRef = ref(db, "candidates/");
         onValue(startCountRef, (snapshot) => {
             const data = snapshot.val();
-            const candidates = Object.keys(data).map(key => ({
-                id: key,
-                ...data[key]
-            }));
-            console.log(candidates);
-            setCandidateList(candidates);
+            if (data != null) {
+                const candidates = Object.keys(data).map(key => ({
+                    id: key,
+                    ...data[key]
+                }));
+                setCandidateList(candidates);
+            }
         });
     }, [])
 
@@ -23,16 +24,17 @@ const CandidateList = () => {
             <ImageBackground source={require("../../assets/candidaeListPage.jpg")}
                 style={{ height: '100%' }}
             >
+                <Text style={styles.infoBar}> Number of candidates present {candidateList.length}</Text>
                 <ScrollView>
                     {
                         candidateList.map((item, index) => {
                             return (
-                                <View key={item.key}>
+                                <View key={index}>
                                     <Text style={styles.item}>
-                                        Contact no.:{item.contactNumber}  {"\n"}
-                                        First Name: {item.firstName} {"\n"}
-                                        Last Name: {item.lastName} {"\n"}
-                                        First Name: {item.firstName} {"\n"}
+                                        Contact no. :{item?.contactNumber}  {"\n"}
+                                        First Name: {item?.firstName} {item?.lastName}{"\n"}
+                                        Fees Paid:  Rs. {item?.feesPaid} {"\n"}
+                                        Date Of Joining: {item?.dateOfJoining}
                                     </Text>
                                 </View>
                             )
@@ -46,9 +48,18 @@ const CandidateList = () => {
 };
 
 const styles = StyleSheet.create({
+    infoBar: {
+        fontSize: 22,
+        backgroundColor: "#FDBD01",
+        marginTop: 20,
+        borderRadius: 50,
+        width: '90%',
+        fontWeight: 'bold',
+        marginLeft: 10
+
+    },
     item: {
         fontSize: 22,
-        backgroundColor: "yellow",
         marginTop: 20,
         padding: 10,
         borderRadius: 30,
